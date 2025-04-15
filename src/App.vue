8d33bd3be3c7be4eaa24a7ce1d6dff215266d117
AndreasYNY <andreasyny@gmx.sg>
2025-04-15 18:14:48 +0700

    add fetch to get latest hash

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,31 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
+import { getCurrentInstance } from 'vue';
 import NavLink from './components/NavLink.vue';
+
+const instance = getCurrentInstance()
+
+let hash: String = ""
+
+// I don't feel like putting everything in the response object here
+// I will just add them later when i need them
+interface githubResponse {
+  after: String
+}
+
+onMounted(async() => {
+  console.log("Main component mounted")
+  // hardcoded api for showcase sake
+  // on real a real app, please have atleast the endpoint (in this case, api.github.com) on .env to be dynamic
+  // I'm not making a URLSearchParam for just few params, to make it simpler
+  // I'm too lazy to implement proper caching
+  const res = await fetch("https://api.github.com/repos/andreasyny/Portfolio/activity?per_page=1&ref=master&activity_type=push")
+  const data: githubResponse[] = await res.json()
+  if (data && res.ok) {
+    hash = data[0].after.slice(0,7)
+    instance?.proxy?.$forceUpdate()
+  }
+})
 </script>
 
 <template>
@@ -9,7 +35,7 @@ import NavLink from './components/NavLink.vue';
         <span class="font-love-light text-9xl grow before:content-['Andreas']"></span>
         <div class="flex flex-col">
           <div class="grow flex justify-end px-2">
-            andreas.diff.my.id@latest
+            andreas.diff.my.id@{{ hash }}
           </div>
           <div class="flex gap-1 px-5">
             <NavLink display="Home" path="/" />
